1357e7ab56ad3f5e2bb26dcb78025c3a47c3eb24
archibishop <36948493+archibishop@users.noreply.github.com>
2018-11-28 17:02:37 +0300

    Add note repository (#3)

--- app/src/main/java/com/example/wagubibrian/architecturecomponentexample/NoteDatabase.java
+++ app/src/main/java/com/example/wagubibrian/architecturecomponentexample/NoteDatabase.java
@@ -1,16 +1,19 @@
 package com.example.wagubibrian.architecturecomponentexample;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 
 @Database(entities = Note.class, version = 1)
 public abstract class NoteDatabase extends RoomDatabase {
 
     private static NoteDatabase instance;
 
-    public NoteDao noteDao;
+    public abstract NoteDao noteDao();
 
     public static synchronized NoteDatabase getInstance(Context context){
         if (instance == null){
@@ -20,5 +23,31 @@ public abstract class NoteDatabase extends RoomDatabase {
         return instance;
     }
 
+    private static RoomDatabase.Callback  roomCallBack = new RoomDatabase.Callback(){
+
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDBAsyncTask(instance).execute();
+        }
+    };
+
+
+    private static class PopulateDBAsyncTask extends AsyncTask<Void, Void, Void> {
+
+        private NoteDao noteDao;
+
+        public PopulateDBAsyncTask(NoteDatabase noteDatabase) {
+            noteDao = noteDatabase.noteDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.insert(new Note("title1", "description1", 1));
+            noteDao.insert(new Note("title3", "description2", 2));
+            noteDao.insert(new Note("title4", "description3", 3));
+            return null;
+        }
+    }
 
 }
